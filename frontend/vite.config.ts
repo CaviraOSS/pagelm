26bffe3a7de648649f314c4c7e470a53855eb84e
Morven <81895400+nullure@users.noreply.github.com>
2025-09-13 21:17:54 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig(({ mode }) => {
       host: env.VITE_FRONTEND_HOST || "localhost",
       port: Number(env.VITE_FRONTEND_PORT) || 4173,
     },
+    envDir: path.resolve(__dirname, ".."),
   };
 });
